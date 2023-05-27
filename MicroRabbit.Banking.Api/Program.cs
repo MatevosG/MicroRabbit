70e933d16d83112f8a2df3537aa8d4805c4cccf6
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2023-05-27 13:15:31 +0400

    Add project files.

---
+++ MicroRabbit.Banking.Api/Program.cs
@@ -0,0 +1,50 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.IoC;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c=> 
+                c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo {Title = "Banking Microservice",Version = "v1" }));
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+RegisterServices();
+
+
+
+var app = builder.Build();
+
+
+void RegisterServices()
+{
+    builder.Services.AddDbContext<BankingDbContext>(options => 
+                                       options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection")));
+
+    DependencyContainer.RegisterServices(builder.Services);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json","Banking Microservice V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime Timestamp { get; set; }
+        public Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
