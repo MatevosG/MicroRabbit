b19447c0adf6df627ba45d4918e5c95021243a13
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2023-06-15 13:00:22 +0400

    done

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,31 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:7277/api/Banking";
+
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(uri, transferContent);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
